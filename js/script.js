f0f88c9838f48d5db077a123e72d10e7e2a09a1b
Shinjhin <huberthubka199@gmail.com>
2021-12-11 18:49:05 +0100

    change variables from var to let & const

--- js/script.js
+++ js/script.js
@@ -1,57 +1,47 @@
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput, argButtonName, button_paper, button_rock, button_scissors;
-
 function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
+    
     function getMoveName(argMoveId) {
-  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-  if (argMoveId == 1) {
-    return 'kamień';
-  } else if (argMoveId == 2) {
-    return 'papier';
-  } else if (argMoveId == 3) {
-    return 'nożyce';
-  } else {
-    printMessage('Wybrano zły numer - wybierz numer od 1 do 3.');
-  }
+        console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+            if (argMoveId == 1) {
+                return 'kamień';
+            } else if (argMoveId == 2) {
+                return 'papier';
+            } else if (argMoveId == 3) {
+                return 'nożyce';
+            }
+    }
+    
+    function displayResult(argPlayerMove, argComputerMove) {
+        console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+        if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+            printMessage('Wygrywasz!');
+        } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+            printMessage('Wygrywasz!');
+        } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+            printMessage('Wygrywasz!');
+        } else if (argPlayerMove == argComputerMove) {
+            printMessage('Remis, spróbuj jeszcze raz!');
+        } else {
+            printMessage('Przegrywasz :(');
+        }
+        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+    }
+    
+    let playerMove = argButtonName;
+    console.log('ruch gracza to: ' + playerMove);
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('wylosowana liczba to: ' + randomNumber);
+    let computerMove = getMoveName(randomNumber);
+    console.log('ruch komputera to: ' + computerMove);
+    displayResult(playerMove, computerMove);
 }
 
-/**
- * Describe this function...
- */
-function displayResult(argPlayerMove, argComputerMove) {
-  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-    printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
-    printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-    printMessage('Wygrywasz!');
-  } else if (argPlayerMove == argComputerMove) {
-    printMessage('Remis, spróbuj jeszcze raz!');
-  } else {
-    printMessage('Przegrywasz :(');
-  }
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-}
-playerMove = argButtonName;
-console.log('wybór ruchu gracza to: ' + playerInput);
-console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
-
-
-}
-button_scissors = document.getElementById('button-scissors');
-button_rock = document.getElementById('button-rock');
-button_paper = document.getElementById('button-paper');
+const button_scissors = document.getElementById('button-scissors');
+const button_rock = document.getElementById('button-rock');
+const button_paper = document.getElementById('button-paper');
 button_rock.addEventListener('click', function(){ buttonClicked('kamień'); });
 button_paper.addEventListener('click', function(){ buttonClicked('papier'); });
 button_scissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
 
-/**
- * Describe this function...
- */
